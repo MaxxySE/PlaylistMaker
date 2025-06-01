530360b4cfbe366657ed5136c52f375fe567150c
MaxxySE <maximrymin15@gmail.com>
2025-06-01 16:09:37 +0400

    20th sprint

--- app/src/main/java/com/example/playlistmaker/sharing/data/NetworkClient.kt
+++ app/src/main/java/com/example/playlistmaker/sharing/data/NetworkClient.kt
@@ -3,5 +3,5 @@ package com.example.playlistmaker.sharing.data
 import com.example.playlistmaker.sharing.data.dto.ApiResponse
 
 interface NetworkClient {
-    fun doRequest(dto: Any): ApiResponse
+    suspend fun doRequest(dto: Any): ApiResponse
 }
